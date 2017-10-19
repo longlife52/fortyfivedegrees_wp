e4f67d4f4ec65d3e7b530e71717f0540fc83aa66
Lisa Long <garrisonridge@gmail.com>
2017-10-19 17:47:33 -0400

    Add html to single.php

--- wp-content/themes/fortyfivedegrees/single.php
+++ wp-content/themes/fortyfivedegrees/single.php
@@ -1,71 +1,55 @@
 <?php get_header(); ?>
 
-	<main role="main">
-	<!-- section -->
-	<section>
+<!-- BILLBOARD -->
+<div class="billboard_black_wrapper">
+    <div class="billboard_black billboard">
+        <div class="blog_title">
+            <h1>3 Ways to Increase Clarity</h1>
+            <h2>problem solving your work and personal life </h2>
+        </div>
+    </div> <!--closer for .billboard_black -->
+</div> <!-- close for .billboard_black_wrapper-->
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<!-- BLOG POST -->
+<div class="blog_content">
+    <div>
+        <p>Don't leave your future to chance. Here are 3 habits that will make a positive difference in your attitude, actions, and results.</p>
+    </div>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="blog_image">
+        <img src="<?php bloginfo('template_url'); ?>/assets/dist/img/clear.png" alt="Increase clarity with focus." title="Increase clarity with focus.">
+    </div>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+    <div class="blog_copy">
+        <h3>1. Focus First</h3>
+            <p>A life coaching tip is to begin with a blank journal, a page which symbolizes a brand new blank day.</p>
+            <p>Jot down rambling thoughts to help you see what's most important in the moment. Then jot down your daily "to do" list. Keep it manageable. No more than 5 items for one day.</p>
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+        <h3>2. Move</h3>
+            <p>Get out of your head and into your body. Sometimes we think too much. When you find yourself under stress, get up and move. Take a walk. Get down on the floor and do 10 sit-ups. Play music and dance. </p>
+            <p>We are created with body, mind, and spirit. In our hectic lives, it's common to spend a vast amount of time in our mind. Use your body. Feel better.</p>
 
-			<!-- post details -->
-			<span class="date">
-				<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
-					<?php the_date(); ?> <?php the_time(); ?>
-				</time>
-			</span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
+        <h3>3. Say No</h3>
+            <p>Whenever you say "yes" to something, you're saying "no" to something else. Stop being all things to all people. Make decisions that algin with your passion. You'll find enery and cheerfulness to profoundly impact your career, your relationships, adn your owne well-being.</p>
+            <p>We only have 24 hours in a day. Some of those need to be used for sleeping. Say "no" to things that are eating up your time and not making a difference in your life.</p>
+    </div> <!-- close of body copy -->
+</div> <!--close of blog content-->
 
-			<?php the_content(); // Dynamic Content ?>
+<!-- fitVids EXPERIMENT for when a client might just dump a video into the site anywhere-->
+<h3 class="center-text">Take a Moment and Watch</h3>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/zSt7k_q_qRU" frameborder="0" allowfullscreen></iframe>
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+<!-- LINKS TO OTHER BLOG ARTICLES -->
+<div class="link_area link_white">
+    <h3>Learn More Ways to Get Clear </h3>
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+    <div class="links">
+        <a href=""><p>7 Problem Solving Insights</p></a>
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+        <a href=""><p>How to Change Focus</p></a>
 
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+        <a href=""><p>A Simple Guide for Finding the Path </p></a>
+    </div>
+</div>
 
 <?php get_footer(); ?>
