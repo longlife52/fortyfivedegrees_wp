b3c9e5d5b43ffcf0b8acc72795d292219f0b5ab5
Lisa Long <garrisonridge@gmail.com>
2017-10-23 20:31:19 -0400

    Delete html from header nav

--- wp-content/themes/fortyfivedegrees/header.php
+++ wp-content/themes/fortyfivedegrees/header.php
@@ -31,17 +31,6 @@
                 <li class="home"><a href="<?php bloginfo('url'); ?>"><img class= "hvr-wobble-to-top-right" src="<?php bloginfo('template_url'); ?>/assets/dist/img/logo_small.png" alt="45 Degrees logo" title="Go to 45 Degrees Home Page"></a></li>
 
                 <?php html5blank_nav(); ?>
-                <!--
-                <li class="coaching"><a href="coaching.html">Coaching</a></li>
-
-                <li class="trainings"><a href="trainings.html">Trainings</a></li>
-
-                <li class="about"><a href="about.html">About</a></li>
-
-                <li class="blog"><a href="blog.html">Blog</a></li>
-
-                <li class="contact"><a href="contact.html">Contact</a></li>
-              -->
             </ul>
         </nav>
 
@@ -61,18 +50,6 @@
                     <li><a href="<?php bloginfo('url'); ?>"><img class="home hvr-wobble-to-top-right" src="<?php bloginfo('template_url'); ?>/assets/dist/img/logo_small.png" alt="45 Degrees logo" title="Go to 45 Degrees Home Page"></a></li>
 
                     <?php html5blank_nav(); ?>
-
-                    <!--
-                    <li><a href="coaching.html" class="coaching">Coaching</a></li>
-
-                    <li><a href="trainings.html" class="trainings">Trainings</a></li>
-
-                    <li><a href="about.html" class="about">About</a></li>
-
-                    <li><a href="blog.html" class="blog">Blog</a></li>
-
-                    <li><a href="contact.html" class="contact">Contact</a></li>
-                  -->
                 </ul>
         </nav>
 
