34a8cd7917f41ba3dd715847549ea76b555eebe2
Lisa Long <garrisonridge@gmail.com>
2017-11-02 13:34:05 -0400

    Add custom-post-type.php for workshop

--- wp-content/themes/fortyfivedegrees/functions/custom-post-types.php
+++ wp-content/themes/fortyfivedegrees/functions/custom-post-types.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*------------------------------------*\
 	Custom Post Types
 \*------------------------------------*/
@@ -45,4 +45,42 @@ function create_post_type_html5()
         )
     );
     */
+
+/* WORKSHOP */
+    register_post_type('workshop', // Register Custom Post Type
+        array(
+        'labels' => array(
+            'name' => __('Workshop', 'html5blank'), // Rename these to suit
+            'singular_name' => __('workshop', 'html5blank'),
+            'add_new' => __('Add New', 'html5blank'),
+            'add_new_item' => __('Add New workshop', 'html5blank'),
+            'edit' => __('Edit', 'html5blank'),
+            'edit_item' => __('Edit workshop', 'html5blank'),
+            'new_item' => __('New workshop', 'html5blank'),
+            'view' => __('View workshop', 'html5blank'),
+            'view_item' => __('View workshop', 'html5blank'),
+            'search_items' => __('Search workshop', 'html5blank'),
+            'not_found' => __('No workshops found', 'html5blank'),
+            'not_found_in_trash' => __('No workshops found in Trash', 'html5blank')
+        ),
+        'public' => true,
+        'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
+        'has_archive' => true,
+        'supports' => array(
+            'title',
+            'excerpt',
+            'thumbnail'
+        ), // Go to Dashboard Custom HTML5 Blank post for supports
+        'menu_icon' => 'dashicons-info',   // http://www.kevinleary.net/wordpress-dashicons-list-custom-post-type-icons/
+        'can_export' => true // Allows export in Tools > Export
+    ));
+    register_taxonomy(
+        'type_of_workshop',    // Taxonomy
+        'workshop',             // Object Type
+        array(
+            'label' => __( 'Type of Workshop' ),
+            'rewrite' => array( 'slug' => 'type' ),
+            'hierarchical' => true, // Is this taxonomy hierarchical like categories or not hierarchical like tags.
+        )
+    );
 }
