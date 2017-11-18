bdb656ff971fb37eac29344c49e778666a9bf7d2
Lisa Long <garrisonridge@gmail.com>
2017-11-18 15:32:42 -0500

    Add workhshop img + ACF for single-workshop.php

--- wp-content/themes/fortyfivedegrees/single-workshop.php
+++ wp-content/themes/fortyfivedegrees/single-workshop.php
@@ -5,40 +5,47 @@
 
 <div>
     <div>
-        <img class="image_full" src="<?php bloginfo('template_url'); ?>/assets/dist/img/cat_large_text.jpg" alt="" title="">
+      <?php $image = get_field('sell_image');
+      if( !empty($image) ): ?>
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="image_full" />
+      <?php endif; ?>
+    </div>
+
+    <div>
+      <h2><?php the_field('sell_title'); ?></h2>
     </div>
 
     <div class="button-box btn_pullup">
-        <a href="mailto:garrisonridge@gmail.com"><p class="ghost_button ghost_btn_white">Register Now</p></a>
+        <a href="<?php the_field('sell_button_link'); ?>" target="blank"><p class="ghost_button ghost_btn_white"><?php the_field('sell_button_label'); ?></p></a>
     </div>
 </div>
 
 <!-- WORKSHOP DETAILS -->
 <div class="workshop">
     <h2 class="center-text">
-      <?php the_title(); ?>
+      <?php the_field('workshop_title'); ?>
     </h2>
-    <h3 class="center-text">Learn 7 Highly Effective Techniques</h3>
+    <h3 class="center-text"><?php the_field('workshop_subtitle'); ?></h3>
 
     <div class="workshop_wrapper">
         <div class="flex_content workshop_description">
 
-          <?php the_content(); ?>
+          <?php the_field('workshop_description'); ?>
 
         </div>
 
         <div class="flex_content workshop_details">
-            <p>DATE - Saturday, 10/21/17</p>
-            <p>TIME - 10 a.m. - Noon </p>
-            <p>PLACE - 1601 Ocean Dr. PVB 32250</p>
-            <p>COST - $40</p>
+            <p>DATE - <?php the_field('workshop_date'); ?></p>
+            <p>TIME - <?php the_field('workshop_time'); ?></p>
+            <p>PLACE - <?php the_field('workshop_location'); ?></p>
+            <p>COST - <?php the_field('workshop_cost'); ?></p>
         </div>
     </div> <!-- closes .flex-wrapper-->
 
     <div class="center_center">
-        <a href="workshop.html">
+        <a href="<?php the_field('sell_button_link'); ?>" target="blank">
             <div>
-                <button class="workshop_button btn_red">Register Now &gt&gt</button>
+                <button class="workshop_button btn_red"><?php the_field('sell_button_label'); ?></button>
             </div>
         </a>
     </div>
